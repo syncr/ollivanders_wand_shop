685cdb40110f35cc7007dff9d9104507684668d9
Jeremy <student@epicodus.com>
2014-08-08 16:58:21 -0700

    provide some UI elements

--- wand_emporium.rb
+++ wand_emporium.rb
@@ -36,18 +36,30 @@ def ui_questions
 end
 
 def ui_wand_selector
-
+  system 'clear'
+  puts "I've considered your responses, but these decisions can take some time. Please be patient as I discuss you with the wands."
+  50.times do |i|
+    print "."
+    sleep(0.05)
+  end
   case Wizard.score
-    when 300..501 then puts "good"
-    when 500..700 then puts "better"
-    when 701..900 then puts "best"
-    else
-    puts "either great or poor"
-end
+    when 0..299 then puts "Oh. Dreadful. It appears you may actually be a muggle. I'll have to call security."
+    when 300..501 then puts "good" #1
+    when 500..700 then puts "better" #2
+    when 701..900 then puts "best" #3
+    when 901..1100 then puts "good" #4
+    when 1301..1500 then puts "better" #5
+    when 1501..1700 then puts "best" #6
+    when 1701..1900 then puts "best" #7
+    when 1901..2100 then puts "best" #8
+    when 2101..2300 then puts "best" #9
+    else puts "Stupendous. Your power rivals the greatest wizards of all time. Perhaps someday you might even challenge the theories of Parlathanx the Bold! What an honor for this shop. Please come back anytime."
+    puts
+  end
+
   puts "wis: #{@wisdom_score}, int: #{@intelligence_score}, loy: #{@loyalty_score}"
   puts Wizard.score
 end
-
 ui_welcome
 
 
